7334574ea3b2eb42b4befeb8752e8ee6aaebd7da
Junior Fernandes <juniorbass8@gmail.com>
2020-06-07 12:35:42 -0300

    Alter file: .gitignore

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js');
-
 const bot = new Discord.Client();
 
-const token = 'NzE5MTk2MDc0NTA1NDA0NDU2.Xtz5pQ.Co4_bMEBIarwM7yj8NUz8cp9UV0';
+const config = require('./config.json');
 
-bot.login(token);
+bot.on('message', message => {
+
+});
+
+bot.login(config.token);
