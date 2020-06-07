ec9871e3ccffd8e6a8f52fc5d4acd4846dee30b4
Junior Fernandes <juniorbass8@gmail.com>
2020-06-07 14:12:39 -0300

    The name of the file called

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const bot = new Discord.Client();
 
 const config = require('./config.json');
 const links = require('./links.json');
-const salas = require('./links.json');
+const salas = require('./salas.json');
 
 // welcome message for user
 bot.on('guildMemberAdd', member => {
-    member.guild.channels.get(salas.geral).send('Seja bem-vindo ' + member.user.username + ', leia atentamente as regras do servidor, e caso tenho alguma segestão, pode falar com um de nossos moderadores, pois sua sugestão é bem vinda.');
+    member.guild.channels.get(salas.geral).send('Seja bem-vindo ' + member.user.username + ' leia atentamente as regras do servidor e caso tenho alguma segestão pode falar com um de nossos moderadores pois sua sugestão é bem vinda!');
     member.send('Bem vindo ao servidor!');
 });
 
 // Remove message for user
 bot.on('guildMemberRemove', member => {
-    member.guild.channels.get(salas.geral).send('Uma pena que foi se foi ' + member.user.username + ' :/');
+    member.guild.channels.get(salas.geral).send('Uma pena que vc se foi ' + member.user.username + ' :/');
 });
 
 bot
